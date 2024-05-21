c340c308bcef7d891ded09b80784b0b7763a8cba
jw <dhks223@naver.com>
2024-05-21 15:47:39 +0900

    kwargs dag 수정6

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -6,7 +6,7 @@ def regist(name,sex,*args):
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}') 
 
-def regist2(*args,name,sex,**kwargs):
+def regist2(name,sex,*args,**kwargs):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}')
