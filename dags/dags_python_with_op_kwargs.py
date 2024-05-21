f207b8d81f7ac4778028e865a42349d6837e7eaf
jw <dhks223@naver.com>
2024-05-21 14:50:50 +0900

    kwarg dag 수정4

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -14,7 +14,7 @@ with DAG(
         task_id='regist2_t1',
         python_callable=regist2,
         op_args=['kr','gj'],
-        op_kwargs1={'name':'jw','sex':'m','email':'dhks223@naver.com','phone':'010'}
+        op_kwargs={'name':'jw','sex':'m','email':'dhks223@naver.com','phone':'010'}
     )
 
     regist2_t1
