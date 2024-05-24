4ea512808d5d78897f3c5ea88426e5d85ac3092e
jw <dhks223@naver.com>
2024-05-24 16:22:42 +0900

    python_macro 수정2

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -10,7 +10,7 @@ with DAG(
 ) as dag:
     # macro 사용
     @task(task_id='task_using_macro',
-          templates_dict={'start_date':'{{(data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, days=1))|ds}}',
+          templates_dict={'start_date':'{{(data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1))|ds}}',
                           'end_date':'{{(data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1))|ds}}'
           } # templates_dict는 키값이고 start,end 는 딕셔너리
             # templates_dict가 키값이 되고 def의 **kwargs로 templates_dict의 값(start_date, end_date) 전체가 전달됨
@@ -30,7 +30,7 @@ with DAG(
 
         data_interval_end = kwargs['data_interval_end'] #kwargs의 defult키값 data_interval_end 값을 꺼내옴
         prev_month_day_first = data_interval_end.in_timezone("Asia/Seoul") + relativedelta(months=-1, day=1)
-        prev_month_day_last = data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + relativedelta(day=-1)
+        prev_month_day_last = data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + relativedelta(days=-1)
         print(prev_month_day_first.strftime('%Y-%m-%d'))
         print(prev_month_day_last.strftime('%Y-%m-%d'))
 
