e701ae4fe711a3a12ed67ca05bbd91a24954311a
jw <dhks223@naver.com>
2024-05-28 16:18:12 +0900

    A

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -22,14 +22,14 @@ with DAG(
              "DATA":"{{ti.xcom_pull(task_ids='python_push')['data']}}",
              "OPTIONS_CNT":"{{ti.xcom_pull(task_ids='python_push')['options_cnt']}}"
         },
-        bash_command="echo $STATUS && echo $DATA && echo $OPTION_CNT"
+        bash_command="echo $STATUS && echo $DATA && echo $OPTIONS_CNT"
     ) 
     
     python_push_xcom() >> bash_pull
 
     bash_push = BashOperator(
         task_id='bash_push',
-        bash_command="echo PUSH_START1"
+        bash_command="echo PUSH_START"
                      "{{ti.xcom_push(key='bash_pushed',value=200)}} && "
                      "echo PUSH_COMPLETE"
     )
@@ -38,7 +38,7 @@ with DAG(
     def python_pull(**kwargs):
         ti = kwargs['ti']
         status_value=ti.xcom_pull(key='bash_pushed')
-        return_value=ti.python_push_xcom_pull(task_ids='bash_push')
+        return_value=ti.xcom_pull(task_ids='bash_push')
         print('status_value:' + str(status_value))
         print('return_value:' + return_value)
 
