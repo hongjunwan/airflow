fc242d02f3a1bc4474b8d4b468362613b4ecbce2
jw <dhks223@naver.com>
2024-05-30 17:11:12 +0900

    a

--- dags/dags_email.py
+++ dags/dags_email.py
@@ -9,7 +9,7 @@ with DAG(
     dag_id="dags_python_email_operator",
     schedule=timedelta(seconds=30),
     start_date= pendulum.datetime (2024, 5, 29, tz="Asia/Seoul"),
-    catchup=False
+    catchup=True
 ) as dag:
     @task(task_id='something_task')
     def some_logic(**kwargs):
