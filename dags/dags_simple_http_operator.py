6bec370e0dcc02244edc733a020a459d017d9460
jw <dhks223@naver.com>
2024-06-05 14:10:38 +0900

    a

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -15,9 +15,10 @@ with DAG(
     '''서울시 공공자전거 대여소 정보'''
     tb_cycle_station_info = SimpleHttpOperator(
         task_id='tb_cycle_station_info',
-        http_conn_id='openapi.seoul.go.kr', # 생성한 connections id 입력
+        http_conn_id='CON_TEST', # 생성한 connections id 입력
         endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10/',
                  # 인증키 직접 입력하지 않고 apikey_openapi_seoul_go_kr 변수에 인증키 작성
+                 # 1/10: 1행~10행 까지
         method='GET',
         headers={'Content-Type': 'application/json',
                         'charset': 'utf-8',
@@ -28,10 +29,10 @@ with DAG(
     @task(task_id='python_2')
     def python_2(**kwargs):
         ti = kwargs['ti']
-        rslt = ti.xcom_pull(task_ids='tb_cycle_station_info')
+        rslt = ti.xcom_pull(task_ids='tb_cycle_station_info') # 해당 task_ids의 return 값 가져옴
         import json
         from pprint import pprint
 
-        pprint(json.loads(rslt))
+        pprint(json.loads(rslt)) # loads(): json 문자열을 파이썬 객체로 변환, http 요청의 전문을 읽을때 많이 사용
         
     tb_cycle_station_info >> python_2()
