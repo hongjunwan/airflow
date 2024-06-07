adc647a78512c5dfa537459a724ae1965f995f86
jw <dhks223@naver.com>
2024-06-07 17:30:20 +0900

    a

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -14,7 +14,7 @@ with DAG(
         dataset_nm='TbCorona19CountStatus',
         path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         # opt는 workspace 경로, files 폴더 생성하고 yaml 파일에서 연결해줘야함, data_interval 부분은 배치종료일자별 폴더 만들기 위해서 사용
-        file_name='TbCorona19CountStatus_{{data_interval_end.in_timezone("Asia/Seoul") + macros.timedelta(hours=8) | ds_nodash }}.csv' # 해당명으로 파일 생성
+        file_name='TbCorona19CountStatus_{{data_interval_end.in_timezone("Asia/Seoul") + macros.datetime.strftime(macros.timedelta(hours=8)) | ds_nodash }}.csv' # 해당명으로 파일 생성
     )
     
     '''서울시 코로나19 백신 예방접종 현황'''
@@ -22,7 +22,7 @@ with DAG(
         task_id='tv_corona19_vaccine_stat_new',
         dataset_nm='tvCorona19VaccinestatNew',
         path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='tvCorona19VaccinestatNew_{{data_interval_end.in_timezone("Asia/Seoul") + macros.timedelta(hours=8) | ds_nodash }}.csv'
+        file_name='tvCorona19VaccinestatNew_{{data_interval_end.in_timezone("Asia/Seoul") + macros.datetime.strftime(macros.timedelta(hours=8)) | ds_nodash }}.csv'
     )
 
     tb_corona19_count_status >> tv_corona19_vaccine_stat_new
